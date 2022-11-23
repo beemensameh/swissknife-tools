ae0cc74753944fbe30c9d96649e509b70741d1c3
beemensameh <41084077+beemensameh@users.noreply.github.com>
2022-11-23 17:32:48 +0000

    update the output message

--- hashing/model.go
+++ hashing/model.go
@@ -59,20 +59,20 @@ func (hashFile *HashFile) Hash() (string, error) {
 
 	switch hashFile.Algorithm {
 	case CRC32:
-		return fmt.Sprintf("%x", crc32.ChecksumIEEE(content)), nil
+		return fmt.Sprintf("CRC32: %x", crc32.ChecksumIEEE(content)), nil
 	case MD5:
-		return fmt.Sprintf("%x", md5.Sum(content)), nil
+		return fmt.Sprintf("MD5: %x", md5.Sum(content)), nil
 	case SHA1:
-		return fmt.Sprintf("%x", sha1.Sum(content)), nil
+		return fmt.Sprintf("SHA1: %x", sha1.Sum(content)), nil
 	case SHA256:
-		return fmt.Sprintf("%x", sha256.Sum256(content)), nil
+		return fmt.Sprintf("SHA256: %x", sha256.Sum256(content)), nil
 	case SHA384:
-		return fmt.Sprintf("%x", sha512.Sum384(content)), nil
+		return fmt.Sprintf("SHA384: %x", sha512.Sum384(content)), nil
 	case SHA512:
-		return fmt.Sprintf("%x", sha512.Sum512(content)), nil
+		return fmt.Sprintf("SHA512: %x", sha512.Sum512(content)), nil
 	case All:
 		return fmt.Sprintf(
-			"crc32:\t%x\nmd5:\t%x\nsha1:\t%x\nsha256:\t%x\nsha384:\t%x\nsha512:\t%x",
+			"CRC32:\t%x\nMD5:\t%x\nSHA1:\t%x\nSHA256:\t%x\nSHA384:\t%x\nSHA512:\t%x",
 			crc32.ChecksumIEEE(content),
 			md5.Sum(content),
 			sha1.Sum(content),
