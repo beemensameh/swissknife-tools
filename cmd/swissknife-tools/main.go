7d43b8bd2306f83957ca12103e58c229c42ac80e
beemensameh <41084077+beemensameh@users.noreply.github.com>
2023-04-13 22:40:18 +0000

    change time to cobra cli

--- cmd/swissknife-tools/main.go
+++ cmd/swissknife-tools/main.go
@@ -2,29 +2,26 @@ package main
 
 import (
 	"log"
-	"os"
 
-	swisshashing "github.com/beemensameh/swissknife-tools/hashing"
-	swissjson "github.com/beemensameh/swissknife-tools/json"
 	swisstime "github.com/beemensameh/swissknife-tools/time"
-	swissuuid "github.com/beemensameh/swissknife-tools/uuid"
-	cli "github.com/urfave/cli/v2"
+	"github.com/spf13/cobra"
+)
+
+var (
+	name    string
+	version string
 )
 
 func main() {
-	app := &cli.App{
-		Name:        "swissknife-tools Tools",
-		Usage:       "A CLI application for many tools",
-		Description: "A quick and amazing tools for speed up your work",
-		Commands: []*cli.Command{
-			swissjson.JSONMinifyCmd,
-			swisstime.TimeNowCmd,
-			swissuuid.GenerateUUIDCmd,
-			swisshashing.HashFileCmd,
-		},
+	rootCmd := &cobra.Command{
+		Use:     name,
+		Short:   "A CLI application for many tools",
+		Long:    "A quick and amazing tools for speed up your work",
+		Version: version,
 	}
+	rootCmd.AddCommand(swisstime.TimeNowCmd)
 
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal("Error in app.Run:\n", err)
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
 	}
 }
